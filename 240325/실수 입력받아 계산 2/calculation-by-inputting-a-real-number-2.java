07baa483437b96830ce8202766440fa5cc8ebe8b
이승석 <106569538+iamseung@users.noreply.github.com>
2024-03-25 09:57:47 +0900

    Title: 실수 입력받아 계산 2, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240325/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(br.readLine());
+        double ft = 1.5;
+
+        System.out.printf("%.2f", a+ft);
+    }
+}
